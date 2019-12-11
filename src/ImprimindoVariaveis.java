1807536dcd6eb6d3dba05a45caf66c9e282f672b
Vitor Mecene <vitor.mecene@hotmail.com>
2019-12-10 23:32:02 -0300

    Aula de java, sobre construção de classe

---
+++ src/ImprimindoVariaveis.java
@@ -0,0 +1,18 @@
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+public class ImprimindoVariaveis {
+    // conmentários
+
+    /*
+    mais comentários
+     */
+
+    /***
+     * Esse e o metodo main
+     * @param args
+     */
+    public static void main(String[] args){
+        int idadeDosPaisParaCadastro = 10;
+        System.out.println("A idade e: "+idadeDosPaisParaCadastro);
+    }
+}
