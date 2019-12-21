64ddf9a6ab9fbad20f630a905014f32e4f9fc2fd
Vitor Mecene <vitor.mecene@hotmail.com>
2019-12-21 10:46:59 -0300

    Conseitos basicos do java, continuação das aulas anteriores

---
+++ src/DeclaraçãoTrabalhista.java
@@ -0,0 +1,11 @@
+public class DeclaraçãoTrabalhista {
+    public static void main (String[] args) {
+        String nome  = "Pedro";
+        String sexo  = "M";
+        String idade = "23";
+        String estadoCivil = "Solteiro";
+        double salario = 3000;
+        System.out.println("O trabalhador(a) " + nome + " do sexo "+sexo+", idade: "+idade+", estado civil: "+estadoCivil+
+                " e salario de R$"+salario+", encontra-se emprergado neste estabelecimento.");
+    }
+}
