9350331c551ddb895f48ba2e35a2720abd9e9b4d
Vitor Mecene <vitor.mecene@hotmail.com>
2019-12-21 11:17:55 -0300

    Aula sobre operadores logicos

---
+++ src/OperadoresLogicos.java
@@ -0,0 +1,14 @@
+public class OperadoresLogicos {
+    public static void main(String[] args){
+        int idade = 18;
+        float salario = 1000f;
+
+        System.out.println("Operador logico E:");
+        System.out.println(idade >= 18 && salario >= 1000);
+        System.out.println(idade >= 18 && salario >= 3000);
+
+        System.out.println("Operador logico OU:");
+        System.out.println(idade >= 18 || salario >= 1000);
+        System.out.println(idade >= 18 || salario >= 3000);
+    }
+}
