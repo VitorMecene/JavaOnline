7a88076c4bc1a2c42f479905c2b4869e7e1204dd
Vitor Mecene <vitor.mecene@hotmail.com>
2019-12-21 11:19:27 -0300

    Aula sobre operadores

--- src/Operadores.java
+++ src/Operadores.java
@@ -6,5 +6,18 @@ public class Operadores {
         System.out.println("A soma é: " + numero1 + numero2); // neste caso o sinal "+" é usado para concatenar;
         System.out.println("A soma é: " + (numero1 + numero2));
         System.out.println("A soma é: " +soma);
+
+        //------------------------------------------------------------------------------------
+
+        int numeroInteiro = 20;
+        long numeroLong = 200000L;
+
+        double numeroDouble = 10d;
+        float numeroFloat = 20f;
+        numeroFloat = (float) numeroDouble;
+        int resto = 20%2;
+        System.out.println("Resto da divisao "+ (20 % 2));
+        System.out.println("Resto da divisao "+ resto);
+        System.out.println(numeroDouble);
     }
 }
