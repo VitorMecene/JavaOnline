2d905cd9b8dd902cb27b97361c74ea7938646b41
Vitor Mecene <vitor.mecene@hotmail.com>
2019-12-23 00:16:47 -0300

    Aula sobre operadores de atribuicao

---
+++ src/OperadoresDeAtribuicao.java
@@ -0,0 +1,13 @@
+public class OperadoresDeAtribuicao {
+    public static void main(String[] args) {
+        // =, -=, +=, *=, /=, %=
+        int salario = 1800;
+        salario = salario + 1000;
+        salario += 1000;
+
+        salario *= 1.0;
+        salario = salario + (int) (salario * 1.0); // salario + 10% do salario
+
+        System.out.println(salario);
+    }
+}
