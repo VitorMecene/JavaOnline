422e9c9733de0e359703f091ea023bd57baf235a
Vitor Mecene <vitor.mecene@hotmail.com>
2020-01-23 17:00:15 -0300

    Interfaces pt 02

--- src/br/com/abc/javacore/interfaces/classes/Carro.java
+++ src/br/com/abc/javacore/interfaces/classes/Carro.java
@@ -1,6 +1,13 @@
 package br.com.abc.javacore.interfaces.classes;
 
 public class Carro implements Tributavel {
+    // do mais restrito <--> para o o mais publico
+    //   private > default > protected > public
+
+    // quanto é deixado em branco esta referencia sub-entende que vc esta utilizando o modificado acesso de pacote
+    // não há problema em deixar em branco o mesmo na classe de interface por causa do padrão da mesma que não pode ser alterado
+    // mas quando o mesmo for sobrescrito em uma classe comum, será obrigatorio evidenciar sua origem
+
     @Override
     public void calculaImposto() {
 
--- src/br/com/abc/javacore/interfaces/test/ProdutoTest.java
+++ src/br/com/abc/javacore/interfaces/test/ProdutoTest.java
@@ -1,10 +1,18 @@
 package br.com.abc.javacore.interfaces.test;
 
 import br.com.abc.javacore.interfaces.classes.Produto;
+import br.com.abc.javacore.interfaces.classes.Tributavel;
 
 public class ProdutoTest {
     public static void main(String[] args) {
         Produto p = new Produto("Notebook",4,3000);
+
+        // é possivel criar uma variavel de referencia para uma interface
+        // Tributavel t;
+
+        // mas não é possivel criar um objeto para a interface, pois a mesma não é estanciavel
+        // Tributavel tr = new Tributavel();
+
         p.calculaFrete();
         p.calculaImposto();
         System.out.println(p);
