564323923067179e527f2705f9aeaa71f73b6d95
Vitor Mecene <vitor.mecene@hotmail.com>
2020-01-22 22:08:05 -0300

    Enumeração pt 01

---
+++ src/br/com/abc/javacore/enumeracao/test/ClienteTest.java
@@ -0,0 +1,12 @@
+package br.com.abc.javacore.enumeracao.test;
+
+import br.com.abc.javacore.enumeracao.TipoCliente;
+import br.com.abc.javacore.enumeracao.classes.Cliente;
+
+public class ClienteTest {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("Anna", TipoCliente.PESSOA_FISICA);
+        System.out.println(cliente);
+
+    }
+}
