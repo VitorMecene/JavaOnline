6d178dcacf26f5955209367468645ffd50f6a9e8
Vitor Mecene <vitor.mecene@hotmail.com>
2020-01-22 21:49:38 -0300

    Modificador Final

---
+++ src/br/com/abc/javacore/modificadorfinal/classes/Subaru.java
@@ -0,0 +1,11 @@
+package br.com.abc.javacore.modificadorfinal.classes;
+
+public class Subaru extends Carro{
+// public class final Subaru
+        // utiliza-se final em classes para as que não poderá sofrer nenhuma alteração, classes "perfeitas"
+
+    //public void imprime(){
+        // Sobrescrever um metodo final gera erro de compiliação,
+        // o que pode ser feito neste caso "forçando a sobrescrita do metodo" é fazer uma sobrecarga nele mesmo
+}
+
---
+++ src/br/com/abc/javacore/modificadorfinal/test/CarroTest.java
@@ -0,0 +1,10 @@
+package br.com.abc.javacore.modificadorfinal.test;
+
+import br.com.abc.javacore.modificadorfinal.classes.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro c = new Carro();
+        //Carro.VELOCIDADE_FINAL = 280; não é possivel alterar o valor de uma  constante
+    }
+}
