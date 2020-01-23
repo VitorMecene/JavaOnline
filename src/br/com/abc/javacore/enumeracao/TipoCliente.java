fddf09f17e0a422f5261dc9fa935ae02cffc0106
Vitor Mecene <vitor.mecene@hotmail.com>
2020-01-22 22:46:47 -0300

    Enumeração pt 03

--- src/br/com/abc/javacore/enumeracao/TipoCliente.java
+++ src/br/com/abc/javacore/enumeracao/TipoCliente.java
@@ -1,7 +1,17 @@
 package br.com.abc.javacore.enumeracao;
 
 public enum TipoCliente {
-    PESSOA_FISICA(1, "Pessoa Fisica"), PESSOA_JURIDICA(2, "Pessoa Juridica");
+    PESSOA_FISICA(1, "Pessoa Fisica"){
+        // sobrescrevendo o metodo getId para PESSOA_FISICA
+        public String getId(){
+            return "C";
+        }
+    }, PESSOA_JURIDICA(2, "Pessoa Juridica"){
+        // sobrescrevendo o metodo getId para PESSOA_JURIDICA
+        public String getId(){
+            return "B";
+        }
+    };
 
     private int tipo;
     private String nome;
@@ -9,6 +19,13 @@ public enum TipoCliente {
     TipoCliente(int tipo, String nome){
         this.tipo = tipo;
         this.nome = nome;
+    }
+
+    // constant specific class body
+    // corpo de classe especifico constante
+
+    public String getId(){
+        return "A";
     }
 
     public String getNome() {
--- src/br/com/abc/javacore/enumeracao/test/ClienteTest.java
+++ src/br/com/abc/javacore/enumeracao/test/ClienteTest.java
@@ -6,6 +6,8 @@ import br.com.abc.javacore.enumeracao.classes.Cliente;
 public class ClienteTest {
     public static void main(String[] args) {
         Cliente cliente = new Cliente("Anna", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.AVISTA);
+        System.out.println(TipoCliente.PESSOA_JURIDICA.getId());
+        System.out.println(TipoCliente.PESSOA_FISICA.getId());
         System.out.println(cliente);
 
     }
