24f60b88fc8db34cf678795c272f5402bc05df92
Vitor Mecene <vitor.mecene@hotmail.com>
2020-02-16 14:52:23 -0300

    Exceptions: bloco finally pt 05

--- src/br/com/abc/javacore/exception/checkedexception/test/CheckedExceptionTest.java
+++ src/br/com/abc/javacore/exception/checkedexception/test/CheckedExceptionTest.java
@@ -5,11 +5,15 @@ import java.io.IOException;
 
 public class CheckedExceptionTest {
     public static void main(String[] args) {
+/*
         try{
             criarArquivo();
         }catch (IOException e){
             e.printStackTrace();
         }
+*/
+
+        abrirArquivo();
     }
 
     public static void criarArquivo() throws IOException {
@@ -21,5 +25,21 @@ public class CheckedExceptionTest {
             throw e;
         }
 
+    }
+
+    public static String abrirArquivo() {
+        try{
+            System.out.println("Abrindo arquivo");
+            System.out.println("Executando leitura do arquivo");
+            //throw new Exception();
+            System.out.println("Escrevendo no arquivo");
+            return "valor";
+        }catch(Exception e){
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando arquivo");
+        }
+        return null;
+        // cria-se sempre um try/catch ou try/finally ou também try/catch/finally
     }
 }
