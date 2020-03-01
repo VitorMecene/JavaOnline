878394534c76ab6589e503d294cb99b7c50b6330
Vitor Mecene <vitor.mecene@hotmail.com>
2020-03-01 13:47:42 -0300

    Tratamento de multiplas exceções pt 06

---
+++ src/br/com/abc/javacore/exception/runtimeexception/test/MultiplasRuntimeExceptionTest.java
@@ -0,0 +1,32 @@
+package br.com.abc.javacore.exception.runtimeexception.test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class MultiplasRuntimeExceptionTest {
+    public static void main(String[] args) {
+        try{
+            throw  new ArrayIndexOutOfBoundsException();
+        }catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+        }catch (IllegalArgumentException e){
+            System.out.println("Dentro do IllegalArgumentException");
+        }catch (ArithmeticException e){
+            System.out.println("Dentro do ArithmeticException");
+        }
+        System.out.println("final do programa");
+        // se as exceções pertencer a mesma filiação, sempre colocar a exceção mais generica por ultimo
+
+        try{
+            talvezLanceExeception();
+        }catch (SQLException e){
+
+        }catch (IOException e){
+
+        }
+    }
+    private static void talvezLanceExeception() throws  SQLException, FileNotFoundException{
+
+    }
+}
